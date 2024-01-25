5bab32a32d0739cc58a4ec55e77e0654e16fa0ed
WarrenAdams8 <warren.codes@outlook.com>
2024-01-25 15:59:58 -0500

    connected wishlist to supabase
    the wishlist page and the book details are now connected to supabase.

--- pages/bookDetails/[id].vue
+++ pages/bookDetails/[id].vue
@@ -5,9 +5,39 @@ import { useBagStore } from '@/stores/bagStore';
 const store = useWishlistStore()
 const BagStore = useBagStore()
 
+const toast = useToast()
+
+
 
 const route = useRoute()
 const { data, pending, error, refresh } = await useFetch(`https://www.googleapis.com/books/v1/volumes/${route.params.id}`)
+
+const supabase = useSupabaseClient()
+async function addToSupabase() {
+    try {
+        const user = useSupabaseUser()
+
+        const updates = {
+            user_id: user.value.id,
+            book_id: data.value.id,
+            book_info: data.value,
+        }
+
+        const { error } = await supabase.from('wishlist_item')
+        .upsert(updates, { returning: 'minimal' })
+        .eq("user_id", user.value.id)
+
+        toast.add({ title: "Book added to wishlist" });
+
+        if (error) throw error
+    } catch (error) {
+        console.log(error.code)
+    }
+
+}
+
+
+
 const img = data.value.volumeInfo.imageLinks.thumbnail + '&fife=w800-h900'
 
 let strippedHtml = ""
@@ -22,6 +52,7 @@ if (data.value.volumeInfo.description) {
 <template>
     <img class="mx-auto" :src="img" />
     <p>{{ strippedHtml }}</p>
-    <UButton label="add to wishlist" class="m-4" @click="store.addToWishlist(data)"  />
-    <UButton label="add to bag" class="m-4" @click="BagStore.addToBag(data)"/>
+    <UButton label="add to supabase" class="m-4" @click="addToSupabase" />
+    <UButton label="add to wishlist" class="m-4" @click="store.addToWishlist(data)" />
+    <UButton label="add to bag" class="m-4" @click="BagStore.addToBag(data)" />
 </template>
--- pages/wishlist.vue
+++ pages/wishlist.vue
@@ -1,15 +1,84 @@
 <script setup>
-
 import { useWishlistStore } from '@/stores/wishlistStore';
 
 const store = useWishlistStore()
-
 const { wishlist } = storeToRefs(store)
-
 const { removeItemFromWishlist } = store
+
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+
+let { data: wishlist_items } = await useAsyncData('wishlist_items', async () => {
+    const { data, error } = await client.from('wishlist_item').select("book_info").eq("user_id", user.value.id)
+
+    const items = []
+    data.forEach((item) => {
+        items.push(item.book_info)
+    })
+    console.log(error)
+
+    return items
+
+})
+
+
+//TODO:Duplicate code
+const toast = useToast()
+async function removeFromSupabase(id) {
+    try {
+
+        const { error } = await client.from('wishlist_item')
+        .delete()
+        .eq("user_id", user.value.id)
+        .eq('book_id', id)
+
+
+        toast.add({ title: "Book removed from wishlist" });
+        
+
+
+        const { data: new_wishlist_items } = await useAsyncData('wishlist_items', async () => {
+            const { data, error } = await client.from('wishlist_item').select("book_info").eq("user_id", user.value.id)
+
+            const items = []
+            data.forEach((item) => {
+                items.push(item.book_info)
+            })
+            console.log(error)
+
+            return items
+
+        })
+
+        wishlist_items = new_wishlist_items
+
+        if (error) throw error
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+
 </script>
 <template>
-    <div class="grid grid-cols-2 md:grid-cols-4 gap-2 p-10">
+    <div v-if="user.role === 'authenticated'" class="grid grid-cols-2 md:grid-cols-4 gap-2 p-10">
+        <UCard v-for="book in wishlist_items">
+            <template #header>
+                <NuxtLink :to="`/bookDetails/${book.id}`">
+                    <p>{{ book.volumeInfo.title }}</p>
+                </NuxtLink>
+            </template>
+            <NuxtLink :to="`/bookDetails/${book.id}`">
+                <img :src="book.volumeInfo.imageLinks.thumbnail" alt="" class="mx-auto">
+            </NuxtLink>
+            <template #footer>
+                <UButton label="Remove" color="red" @click="removeFromSupabase(book.id)" />
+            </template>
+        </UCard>
+    </div>
+    <div v-else class="grid grid-cols-2 md:grid-cols-4 gap-2 p-10">
         <UCard v-for="book in wishlist">
             <template #header>
                 <NuxtLink :to="`/bookDetails/${book.id}`">
